9c4eb2d9ba08726d5ec065f92c9f97a6aead5c45
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-12-28 22:42:29 -0300

    feat: complete frontend implementation of the form

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,37 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
 });
 
 export const metadata = {
   title: "Ticket Generator",
-  description: "",
+  description: "Description",
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetBrainsMono.className} antialiased`}
       >
         {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              // Remove atributos dinâmicos adicionados por extensões como Dark Reader
+              document.querySelectorAll('[data-darkreader-mode]').forEach(el => {
+                el.removeAttribute('data-darkreader-mode');
+                el.removeAttribute('data-darkreader-scheme');
+                el.removeAttribute('data-darkreader-proxy-injected');
+              });
+            `,
+          }}
+        />
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image"
 import { InputWithLabel } from "../components/inputWithLabel";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-slate-950 flex flex-col items-center justify-evenly text-zinc-50">
-      <div className="flex flex-col items-center justify-evenly -my-60 gap-4">
-        <strong className="text-xl">Coding Conf</strong>
-        <h1 className="w-[28rem] text-4xl font-bold text-center">Your Journey to Coding Conf 2025 Starts Here!</h1>
+    <div className="w-screen h-screen bg-slate-950 flex flex-col items-center justify-center text-zinc-50 background">
+      <div className="flex flex-col items-center justify-evenly gap-4 mb-10">
+        <Image 
+          className="-ml-1"
+          src="/logo-full.svg"
+          width={192}
+          height={40}
+          alt="Coding Conf Logo"
+        />
+        <h1 className="w-[50rem] text-5xl font-bold text-center">Your Journey to Coding Conf 2025 Starts Here!</h1>
         <p className="text-zinc-300">Secure your spot at next year's biggest coding conference.</p>
       </div>
       <form className="w-1/3 flex flex-col items-center gap-4">
