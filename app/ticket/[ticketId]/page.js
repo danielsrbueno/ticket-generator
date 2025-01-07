5469548fbd5a642bbe049fccc4feb56134852989
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-01-07 14:40:05 -0300

    feat: add loading skeleton effect

--- app/ticket/[ticketId]/page.js
+++ app/ticket/[ticketId]/page.js
@@ -7,6 +7,8 @@ import api from "@/services/api"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Home } from "lucide-react"
+import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton"
 
 export default function Ticket() {
     const { ticketId } = useParams()
@@ -70,8 +72,8 @@ export default function Ticket() {
                 height={40}
                 alt="Coding Conf Logo"
             />
-            <h1 className="text-4xl font-bold w-[36rem] text-center">Congrats, <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#f57463] to-zinc-50 font-extrabold">{fullname}! </span>Your ticket is ready.</h1>
-            <p className="text-zinc-50/90 font-extralight w-[28rem] text-center -mt-5">We've emailed your ticket to {email} and will send updates in the run up to the event.</p>
+            <h1 className="text-4xl font-bold w-[36rem] text-center">Congrats, <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#f57463] to-zinc-50 font-extrabold">{ticket?.userName ? fullname + "!" : <Skeleton height={36} width={280} baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8}/>} </span>Your ticket is ready.</h1>
+            <p className="text-zinc-50/90 font-extralight w-[28rem] text-center -mt-5">We've emailed your ticket to {email || <Skeleton height={20} width={160} baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8} inline={true}/>} and will send updates in the run up to the event.</p>
             <div className="ticketshape w-[32.5rem] h-64 flex flex-col backdrop-blur-sm justify-around px-6">
                 <div className="text-zinc-50/90 font-thin text-xs flex flex-col gap-2">
                     <Image
@@ -84,22 +86,26 @@ export default function Ticket() {
                     <p className="ml-12">Jan 31, 2025 / Austin, TX</p>
                 </div>
                 <div className="flex gap-4">
-                    <img
-                        className="rounded-lg"
-                        width={80}
-                        alt="Github Avatar"
-                        src={avatarUrl || "/favicon.ico"}
-                    />
+                    {avatarUrl ? 
+                        <img
+                            className="rounded-lg"
+                            width={80}
+                            alt="Github Avatar"
+                            src={avatarUrl || "/favicon.ico"}
+                        /> : 
+                        <Skeleton height={80} width={80} baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8}/>
+                    }
+                    
                     <div className="h-full flex flex-col gap-2 py-2">
-                        <p className="font-semibold text-lg">{fullname}</p>
+                        <p className="font-semibold text-lg">{fullname || <Skeleton height={24} width={160} baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8}/>}</p>
                         <span className="flex items-center gap-1 text-xs">
                             <Github width={16}/>
-                            <p>@{github}</p>
+                            <p>@{github || <Skeleton height={16} width={120}  baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8}/>}</p>
                         </span>
                     </div>
                 </div>
                 <div className="absolute left-[27.46rem] flex items-center rotate-90 text-zinc-50/60 text-xl font-extralight">
-                    <p>{`#${ticketcode}`}</p>
+                    <p>{ticket?.ticketId ? `#${ticketcode}` : <Skeleton height={20} width={60} baseColor="#ffffff0f" highlightColor="#ffffff99" duration={0.8}/>}</p>
                 </div>
             </div>
         </div>
